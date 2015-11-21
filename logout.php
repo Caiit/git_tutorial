a9459ed4c94fd50a1f68f676e33938eef4cd63aa
Houda <houda1996@hotmail.com>
2015-11-21 16:46:25 +0100

    Kleine aanpassing in de logout file als test

--- logout.php
+++ logout.php
@@ -6,6 +6,7 @@
 	$currentBack = $_SERVER['HTTP_REFERER'];
 	
 	/* If the user is on his page, he is redirected to the index page, which is the homepage. */
+	/* Extra changement to see what it does.*/
 	if(strpos($currentBack, 'mypage.php')) {
 		$redirect = "https://localhost/website/index.php";	
 	} else {
